bebd86f207d3a24ebeda1663a33ad9cad0e20282
killerbeno111-jpg <killerbeno111@gmail.com>
2026-09-11 22:11:03 -0400

    Update MainActivity.kt

--- app/src/main/java/com/poultryfoodsafety/openknowledge/MainActivity.kt
+++ app/src/main/java/com/poultryfoodsafety/openknowledge/MainActivity.kt
@@ -1,1 +1,20 @@
+package com.poultryfoodsafety.openknowledge
 
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val title = TextView(this).apply {
+            text = "Poultry Food Safety Open Knowledge\n\nWelcome!\n\nBroiler Processing QC Learning App"
+            textSize = 22f
+            setPadding(32, 64, 32, 32)
+        }
+
+        setContentView(title)
+    }
+}
